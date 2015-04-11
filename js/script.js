d328324830179172aa0dc7834f0b5b0f2af725e4
Karen <karenpenglabs@gmail.com>
2015-04-11 16:04:21 -0400

    making some progress on both sides

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
- // window.onload = function () {
- //   var video = document.getElementById('video');
- //   var canvas = document.getElementById('canvas');
- //   var context = canvas.getContext('2d');
-
- //   // var tracker = new tracking.ObjectTracker(['face', 'eye']);
- //   var tracker = new tracking.ObjectTracker('eye');
- //   // tracker.setInitialScale(4);
- //   // tracker.setStepSize(2);
- //   // tracker.setEdgesDensity(0.1);
-
- //   tracking.track('#video', tracker, {
- //     camera: true
- //   });
-
- //   tracker.on('track', function (event) {
- //     context.clearRect(0, 0, canvas.width, canvas.height);
- //     context.fillStyle = 'black';
- //     context.fillRect(0, 0, canvas.width, canvas.height);
-
- //     event.data.forEach(function (rect) {
- //       context.beginPath();
- //       context.arc(rect.x + rect.width / 2, rect.y + rect.width / 2, rect.width / 4, 0, 2 * Math.PI, false);
- //       context.fillStyle = 'white';
- //       context.fill();
- //       // context.strokeStyle = '#a64ceb';
- //       // context.strokeRect(rect.x, rect.y, rect.width, rect.height);
- //       // context.font = '11px Helvetica';
- //       // context.fillStyle = "#fff";
- //       // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
- //       // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
- //     });
- //   });
-
- // };
-
 var vid = document.getElementById('videoel');
 var overlay = document.getElementById('overlay');
 var overlayCC = overlay.getContext('2d');
@@ -91,40 +55,70 @@ function startVideo() {
   drawLoop();
 }
 
+var frameCount = 0;
+var lastPosL = [];
+var lastPosR = [];
+
 function drawLoop() {
   requestAnimFrame(drawLoop);
+
+  frameCount ++;
+
   overlayCC.clearRect(0, 0, 400, 300);
   //psrElement.innerHTML = "score :" + ctrack.getScore().toFixed(4);
 
   if (ctrack.getCurrentPosition()) {
     var positions = ctrack.getCurrentPosition();
+
+    if(frameCount % 30 === 0){
+      lastPosL = positions[27];
+      lastPosR = positions[32];
+    }
+
+    overlayCC.strokeStyle = 'red';
+    overlayCC.beginPath();
+    overlayCC.moveTo(lastPosL[0], lastPosL[1]);
+    overlayCC.lineTo(positions[27][0], positions[27][1]);
+    overlayCC.stroke();
+
+    overlayCC.beginPath();
+    overlayCC.moveTo(lastPosR[0], lastPosR[1]);
+    overlayCC.lineTo(positions[32][0], positions[32][1]);
+    overlayCC.stroke();
+
     // ctrack.draw(overlay);
     overlayCC.fillStyle = 'white';
     overlayCC.fillRect(positions[27][0], positions[27][1], 3, 3);
     overlayCC.fillRect(positions[32][0], positions[32][1], 3, 3);
 
-    overlayCC.fillStyle = 'green';
+    // overlayCC.fillStyle = 'green';
 
-    overlayCC.fillRect(positions[63][0], positions[63][1], 3, 3);
-    overlayCC.fillRect(positions[24][0], positions[24][1], 3, 3);
-    overlayCC.fillRect(positions[64][0], positions[64][1], 3, 3);
-    overlayCC.fillRect(positions[23][0], positions[23][1], 1, 1);
+    // overlayCC.fillRect(positions[63][0], positions[63][1], 3, 3);
+    // overlayCC.fillRect(positions[24][0], positions[24][1], 3, 3);
+    // overlayCC.fillRect(positions[64][0], positions[64][1], 3, 3);
+    // overlayCC.fillRect(positions[23][0], positions[23][1], 1, 1);
 
-    overlayCC.fillRect(positions[66][0], positions[66][1], 3, 3);
-    overlayCC.fillRect(positions[26][0], positions[26][1], 3, 3);
-    overlayCC.fillRect(positions[65][0], positions[65][1], 3, 3);
-    overlayCC.fillRect(positions[25][0], positions[25][1], 1, 1);
+    // overlayCC.fillRect(positions[66][0], positions[66][1], 3, 3);
+    // overlayCC.fillRect(positions[26][0], positions[26][1], 3, 3);
+    // overlayCC.fillRect(positions[65][0], positions[65][1], 3, 3);
+    // overlayCC.fillRect(positions[25][0], positions[25][1], 1, 1);
 
-    overlayCC.fillRect(positions[68][0], positions[68][1], 3, 3);
-    overlayCC.fillRect(positions[29][0], positions[29][1], 3, 3);
-    overlayCC.fillRect(positions[67][0], positions[67][1], 3, 3);
-    overlayCC.fillRect(positions[30][0], positions[30][1], 1, 1);
+    // overlayCC.fillRect(positions[68][0], positions[68][1], 3, 3);
+    // overlayCC.fillRect(positions[29][0], positions[29][1], 3, 3);
+    // overlayCC.fillRect(positions[67][0], positions[67][1], 3, 3);
+    // overlayCC.fillRect(positions[30][0], positions[30][1], 1, 1);
 
-    overlayCC.fillRect(positions[69][0], positions[69][1], 3, 3);
-    overlayCC.fillRect(positions[31][0], positions[31][1], 3, 3);
-    overlayCC.fillRect(positions[70][0], positions[70][1], 3, 3);
-    overlayCC.fillRect(positions[28][0], positions[28][1], 1, 1);
+    // overlayCC.fillRect(positions[69][0], positions[69][1], 3, 3);
+    // overlayCC.fillRect(positions[31][0], positions[31][1], 3, 3);
+    // overlayCC.fillRect(positions[70][0], positions[70][1], 3, 3);
+    // overlayCC.fillRect(positions[28][0], positions[28][1], 1, 1);
   }
+}
+
+function blickDetection(pos, lastPos){
+  // if(lastPos - pos){
+
+  // }
 }
 
 // update stats on every iteration
